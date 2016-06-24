af44d157b9d32cf2ea17160b7bdff29972297709
wildabenny <strzelczykster@gmail.com>
2016-06-24 13:39:23 +0200

    removed calendar bundle

--- TaskPlanner/app/AppKernel.php
+++ TaskPlanner/app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
