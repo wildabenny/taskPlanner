39206f70db905341d1c2825fd809722f5b6539da
wildabenny <strzelczykster@gmail.com>
2016-08-03 23:24:55 +0200

    added some js

--- TaskPlanner/web/js/finishTask.js
+++ TaskPlanner/web/js/finishTask.js
@@ -2,8 +2,10 @@ $(function () {
     var button = $("#button1");
 
     button.on("click", function () {
+        var lista = $(this).closest("li");
         console.log("działa");
         $(this).closest('p').html('<strong>Task Is Finished</strong>');
+        console.log(lista.data());
 
     });
 });
